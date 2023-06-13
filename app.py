7615cf1166f19e602bf8f49b4c395765a05ce83d
eumduryenog <136227473+eumduryenog@users.noreply.github.com>
2023-06-13 11:16:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from nbconvert import PythonExporter
 import os.path
 
 # 애플리케이션 제목
@@ -9,6 +8,8 @@ st.title("경기과기대 201821018 음두령")
 st.header("제어공학 기말고사")
 # 페이지 소제목
 st.subheader("2번 문제")
+
+from nbconvert import PythonExporter
 
 # 201821018_eum.ipynb 파일을 Python 코드로 변환
 notebook_file = '201821018_eum.ipynb'
